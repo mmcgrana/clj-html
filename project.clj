64dc9345edac048f8e6e80d92b8019d0bb530e46
Mark McGranaghan <mmcgrana@gmail.com>
2010-02-23 21:46:16 -0500

    Update to depend on recent and stable Clojure and Clojure Contrib.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-html "0.1.0-SNAPSHOT"
   :description "A Clojure library for expanding expressive HTML templates into efficient code."
   :url "http://github.com/mmcgrana/clj-html"
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [clj-unit "0.1.0-SNAPSHOT"]])
